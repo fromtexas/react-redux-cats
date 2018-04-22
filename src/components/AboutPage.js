c8ebbf9085fd481399d8698b62f09030ab37d59e
fromtexas <telures@yandex.ru>
2018-04-22 17:22:01 +0300

    about

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,6 +3,9 @@ import React from 'react';
 const AboutPage = () => (
     <div className='about'>
         <p>Random pictures of cats</p>
+        <p>The most familiar cats are the domestic shorthair and the domestic longhair, which are really mixtures of different breeds.</p>
+        <p>Cat breeds differ in looks, coat length, and other characteristics but vary relatively little in size.</p>
+        <p>On average, only 5 to 10 pounds separate the smallest and largest domestic breeds of cats.</p>
     </div>
 );
 
